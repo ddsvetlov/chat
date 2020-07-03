86a9b6714c17c208da581904961fbcc077db0425
ddsvetlov <dankasvetlov@gmail.com>
2020-07-03 19:53:13 +0300

    add role, bootstrap, refactor smth

--- src/main/java/com/example/chat/controller/UserController.java
+++ src/main/java/com/example/chat/controller/UserController.java
@@ -1,106 +1,63 @@
-//package com.example.chat.controller;
-//
-//import com.example.chat.domain.Role;
-//import com.example.chat.domain.User;
-//import com.example.chat.service.UserSevice;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.Map;
-//
-//@Controller
-//@RequestMapping("/user")
-//public class UserController {
-//    @Autowired
-//    private UserSevice userSevice;
-//
-//    @PreAuthorize("hasAuthority('ADMIN')")
-//    @GetMapping
-//    public String userList(Model model) {
-//        model.addAttribute("users", userSevice.findAll());
-//
-//        return "userList";
-//    }
-//
-//    @PreAuthorize("hasAuthority('ADMIN')")
-//    @GetMapping("{user}")
-//    public String userEditForm(@PathVariable User user, Model model) {
-//        model.addAttribute("user", user);
-//        model.addAttribute("roles", Role.values());
-//
-//        return "userEdit";
-//    }
-//
-//    @PreAuthorize("hasAuthority('ADMIN')")
-//    @PostMapping
-//    public String userSave(
-//            @RequestParam String username,
-//            @RequestParam Map<String, String> form,
-//            @RequestParam("userId") User user
-//    ) {
-//        userSevice.saveUser(user, username, form);
-//
-//        return "redirect:/user";
-//    }
-//
-//    @GetMapping("profile")
-//    public String getProfile(Model model, @AuthenticationPrincipal User user) {
-//        model.addAttribute("username", user.getUsername());
-//        model.addAttribute("email", user.getEmail());
-//
-//        return "profile";
-//    }
-//
-//    @PostMapping("profile")
-//    public String updateProfile(
-//            @AuthenticationPrincipal User user,
-//            @RequestParam String password,
-//            @RequestParam String email
-//    ) {
-//        userSevice.updateProfile(user, password, email);
-//
-//        return "redirect:/user/profile";
-//    }
-//
-//    @GetMapping("subscribe/{user}")
-//    public String subscribe(
-//            @AuthenticationPrincipal User currentUser,
-//            @PathVariable User user
-//    ) {
-//        userSevice.subscribe(currentUser, user);
-//
-//        return "redirect:/user-messages/" + user.getId();
-//    }
-//
-//    @GetMapping("unsubscribe/{user}")
-//    public String unsubscribe(
-//            @AuthenticationPrincipal User currentUser,
-//            @PathVariable User user
-//    ) {
-//        userSevice.unsubscribe(currentUser, user);
-//
-//        return "redirect:/user-messages/" + user.getId();
-//    }
-//
-//    @GetMapping("{type}/{user}/list")
-//    public String userList(
-//            Model model,
-//            @PathVariable User user,
-//            @PathVariable String type
-//    ) {
-//        model.addAttribute("userChannel", user);
-//        model.addAttribute("type", type);
-//
-//        if ("subscriptions".equals(type)) {
-//            model.addAttribute("users", user.getSubscriptions());
-//        } else {
-//            model.addAttribute("users", user.getSubscribers());
-//        }
-//
-//        return "subscriptions";
-//    }
-//}
+package com.example.chat.controller;
+
+import com.example.chat.domain.Role;
+import com.example.chat.domain.User;
+import com.example.chat.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
+public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping
+    public String userList(Model model) {
+        model.addAttribute("users", userRepo.findAll());
+
+        return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+
+        return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") User user
+    ) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepo.save(user);
+
+        return "redirect:/user";
+    }
+}
--- src/main/java/com/example/chat/domain/Message.java
+++ src/main/java/com/example/chat/domain/Message.java
@@ -1,10 +1,6 @@
 package com.example.chat.domain;
 
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -15,12 +11,31 @@ public class Message {
     private String text;
     private String tag;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    private String filename;
+
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
+        this.author = user;
         this.text = text;
         this.tag = tag;
+    }
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 
     public void setText(String text) {
@@ -45,5 +60,13 @@ public class Message {
 
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
